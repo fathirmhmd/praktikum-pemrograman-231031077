a081cf16d9ab2a5823cc6c7d5a7b7d77fa18d635
fathirmhmd <153782302+fathirmhmd@users.noreply.github.com>
2023-12-14 09:11:36 +0800

    Add files via upload

---
+++ praktikum-c1.py
@@ -0,0 +1,37 @@
+print('praktikum-1')
+print()
+print('Nama Lengkap: Fathir Muhammad')
+print('Nim         : 231031077')
+print('prodi       : sistem informasi')
+
+angkatan = 2023
+print('Angkatan adalah',angkatan)
+print()
+a = 20
+print('Data adalah =',a)
+print('Tipe data adalah =',type(a))
+
+print()
+b = 20.0
+print('Data adalah =',b)
+print('Tipe data adalah =',type(b))
+
+kampus = 'Institut Teknologi BJ Habibie'
+print('Data adalah =',kampus)
+print('Tipe data adalah =',type(kampus))
+
+print()
+log = True
+print('Data adalah =',log)
+print('Tipe data adalah =',type(log))
+
+print()
+c = complex(3,4)
+print('Data adalah =',c)
+print('Tipe data adalah =',type(c))
+
+print()
+p = 20
+q = 5
+hasil = p + q
+print('Hasil',p,'+',q,'=',hasil)
